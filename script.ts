6bd814bd937c5c8f3b06c9f5c2d8cd6c6a08f126
Vaclav Valentine <152690177+VaclavKey@users.noreply.github.com>
2025-03-04 00:01:59 +0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,21 +1,20 @@
-let selectedScore: number = 0;
-
-document.querySelectorAll<HTMLButtonElement>(".rating-button").forEach(button => {
-  button.addEventListener("click", () => {
-    selectedScore = parseInt(button.textContent || "0", 10); // Преобразуем текстовое содержимое в число
-    document.querySelectorAll<HTMLButtonElement>(".rating-button").forEach(btn => btn.classList.remove("selected"));
-    button.classList.add("selected");
-  });
+"use strict";
+var _a;
+let selectedScore = 0;
+document.querySelectorAll(".rating-button").forEach(button => {
+    button.addEventListener("click", () => {
+        selectedScore = parseInt(button.textContent || "0", 10); // Convert text content to number
+        document.querySelectorAll(".rating-button").forEach(btn => btn.classList.remove("selected"));
+        button.classList.add("selected");
+    });
 });
-
-document.querySelector<HTMLElement>(".submit-button")?.addEventListener("click", () => {
-  const ratingState = document.querySelector<HTMLElement>(".rating-state");
-  const thankYouState = document.querySelector<HTMLElement>(".thank-you-state");
-  const scoreElement = document.querySelector<HTMLElement>("#score");
-
-  if (ratingState && thankYouState && scoreElement) {
-    ratingState.style.display = "none";
-    thankYouState.style.display = "flex";
-    scoreElement.textContent = selectedScore.toString();
-  }
+(_a = document.querySelector(".submit-button")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    const ratingState = document.querySelector(".rating-state");
+    const thankYouState = document.querySelector(".thank-you-state");
+    const scoreElement = document.querySelector("#score");
+    if (ratingState && thankYouState && scoreElement) {
+        ratingState.style.display = "none";
+        thankYouState.style.display = "flex";
+        scoreElement.textContent = selectedScore.toString();
+    }
 });
